9f1e98e4d409457eb237f9bfc6d980bc95711da8
sajjan.mishra <sajjan.mishra@arq.group>
2020-08-20 22:44:07 +1000

    Adding modal from material ui

--- src/App.js
+++ src/App.js
@@ -2,9 +2,38 @@ import React, {useState, useEffect } from 'react';
 import './App.css';
 import Post from './Post';
 import { db } from './firebase';
+import { makeStyles} from '@material-ui/core/styles';
+import Modal from '@material-ui/core/Modal';
+import { Button } from '@material-ui/core';
+
+function getModalStyle() {
+  const top = 50;
+  const left = 50;
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: 'absolute',
+    width: 400,
+    backgroundColor: theme.palette.background. paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
 
 function App() {
+  const classes = useStyles();
+  const [modalStyle] = useState(getModalStyle);
+
   const [posts, setPosts] = useState([ ]);
+  const [open, setOpen] = useState(false);
 
   // UseEffect Runs a peice of code based on a specific conditions
 
@@ -12,13 +41,29 @@ function App() {
     // this is where the code runs
     db.collection('posts').onSnapshot(snapshot => {
         // everytime a new post is added, this code fire
-      setPosts(snapshot.docs.map(doc => doc.data()));
+      setPosts(snapshot.docs.map(doc => ({
+        id: doc.id, 
+        post: doc.data()
+      })));
     })
 
   }, []);
 
+  const signUp = (event) => {
+
+  }
+
   return (
     <div className="app">
+      <Modal
+        open={open}
+        onClose={() => setOpen(false)}
+      >
+        <div style={modalStyle} className={classes.paper}>
+          <h2>I am a modal</h2>
+        </div>
+      </Modal>
+
       <div className="app__header">
         <img
           className="app__headerImage"
@@ -27,11 +72,13 @@ function App() {
         />
       </div>
       
-      <h1>Hey guys! Let's build an Instagram Clone app with React :rocket: !</h1>
+      <Button onClick={() => setOpen(true)}>Sign Up</Button>
+
+      <h1>Hey guys! Let's build an Instagram Clone app with React!</h1>
 
       {
-        posts.map(post => (
-          <Post username={post.username} caption={post.caption} imageUrl={post.imageUrl} />
+        posts.map(({id, post}) => (
+          <Post key={id} username={post.username} caption={post.caption} imageUrl={post.imageUrl} />
         ))
       }
 
